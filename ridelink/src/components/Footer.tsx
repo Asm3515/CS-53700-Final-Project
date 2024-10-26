8f1115e2fe9284526ff14514fe3cc5ab0a53730d
pallavibakale <pallavibakale09@gmail.com>
2024-10-26 12:11:39 -0400

    updated footer

--- ridelink/src/components/Footer.tsx
+++ ridelink/src/components/Footer.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <p>Share a ride !</p>
-      </footer>
-  )
-}
+    <>
+      {/* Gold Divider */}
+      <div className="w-full h-[1px] bg-yellow-500 mb-4"></div>
 
-export default Footer
+      <footer className="row-start-3 flex flex-col items-center p-6 bg-black text-white md:flex-row md:justify-between">
+        {/* Links */}
+        <div className="flex flex-col items-center gap-2 mb-4 md:mb-0 md:items-start">
+          <a href="/book-ride" className="text-lg hover:underline">
+            Book a Ride
+          </a>
+          <a href="/give-ride" className="text-lg hover:underline">
+            Give a Ride
+          </a>
+        </div>
+
+        {/* Center Text */}
+        <p className="text-center text-lg mb-4 md:mb-0">Share a ride!</p>
+
+        {/* Logo with Link to Homepage */}
+        <div className="flex items-center">
+          <Link href="/" passHref>
+            <img
+              src="/ridelink-logo-black.webp"
+              alt="Company Logo"
+              className="h-10 w-auto cursor-pointer"
+            />
+          </Link>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
