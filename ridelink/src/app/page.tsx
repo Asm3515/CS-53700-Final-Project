162b67f94959e105f55707b17fedb4c725477a94
pallavibakale <pallavibakale09@gmail.com>
2024-12-08 03:44:33 -0500

    embedded map

--- ridelink/src/app/page.tsx
+++ ridelink/src/app/page.tsx
@@ -34,11 +34,7 @@ export default async function Home() {
           )}
         </div>
         <div className="mt-8 md:mt-0 md:flex-1">
-          <img
-            src="/maps.jpg"
-            alt="maps-static"
-            className="rounded-lg shadow-lg w-full h-auto object-cover transition-transform transform hover:scale-105 duration-300"
-          />
+          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d24043.906684251593!2d-85.098496!3d41.1238644!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1733451815628!5m2!1sen!2sus" width="600" height="450" loading="lazy" className="rounded-lg shadow-lg object-cover hover:scale-105 duration-300 w-full"></iframe>
         </div>
       </div>
 
