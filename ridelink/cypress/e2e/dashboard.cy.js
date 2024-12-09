74f9e93365f506806d1952f820927286b6cc380a
pallavibakale <pallavibakale09@gmail.com>
2024-12-09 08:14:26 -0500

    added dashboard redirection test case

--- ridelink/cypress/e2e/dashboard.cy.js
+++ ridelink/cypress/e2e/dashboard.cy.js
@@ -1,12 +1,21 @@
-describe('Dashboard Features', () => {
-    
-    it('should display Passenger Dashboard correctly', () => {
-      cy.visit('https://ridelink-public.vercel.app/passenger/dashboard');
-      cy.contains('Passenger Dashboard').should('be.visible');
-    });
-  
-    it('should display Driver Dashboard correctly', () => {
-      cy.visit('https://ridelink-public.vercel.app/drivers/dashboard');
-      cy.contains('Driver Dashboard').should('be.visible');
-    });
+describe("Dashboard Features", () => {
+  it("should display Passenger Dashboard correctly", () => {
+    cy.visit("https://ridelink-public.vercel.app/passenger/dashboard");
+    cy.contains("Passenger Dashboard").should("be.visible");
   });
+
+  it("should display Driver Dashboard correctly", () => {
+    cy.visit("https://ridelink-public.vercel.app/drivers/dashboard");
+    cy.contains("Driver Dashboard").should("be.visible");
+  });
+
+  it("should redirect to Passenger Dashboard on button click", () => {
+    cy.visit("https://ridelink-public.vercel.app/");
+
+    // Click the "Get Started" button
+    cy.get("button").contains("Get Started").click();
+
+    // Verify redirection to the Passenger Dashboard
+    cy.url().should("not.include", "/dashboard");
+  });
+});
