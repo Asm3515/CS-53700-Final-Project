d0dc455c553c01d9f3ff4a31fb4b14a210007f89
pallavibakale <pallavibakale09@gmail.com>
2024-12-09 08:32:45 -0500

    added dashboard navigation test cases

--- ridelink/cypress/e2e/dashboard.cy.js
+++ ridelink/cypress/e2e/dashboard.cy.js
@@ -9,9 +9,22 @@ describe("Dashboard Features", () => {
     cy.contains("Driver Dashboard").should("be.visible");
   });
 
-  it("should redirect to Passenger Dashboard on button click", () => {
+  it("should redirect to Sign In page when user is not logged in", () => {
     cy.visit("https://ridelink-public.vercel.app/");
     cy.get("button").contains("Get Started").click();
     cy.url().should("not.include", "/dashboard");
+    cy.contains("Sign in to RideLink").should("be.visible");
+  });
+
+  it("should redirect to the Sign In page when not authenticated for ride link", () => {
+    cy.visit("https://ridelink-public.vercel.app/passenger/dashboard");
+    cy.url().should("include", "/sign-in");
+    cy.contains("Sign in to RideLink").should("be.visible");
+  });
+
+  it("should redirect to the Sign In page when not authenticated for drive link", () => {
+    cy.visit("https://ridelink-public.vercel.app/drivers/dashboard");
+    cy.url().should("include", "/sign-in");
+    cy.contains("Sign in to RideLink").should("be.visible");
   });
 });
