e3867839e68fbf216d0b7ae194d2506098b7e898
pallavibakale <pallavibakale09@gmail.com>
2024-12-09 09:34:13 -0500

    added ride request test case

--- ridelink/cypress/e2e/ridemanagement.cy.js
+++ ridelink/cypress/e2e/ridemanagement.cy.js
@@ -93,10 +93,55 @@ describe("Ride Management", () => {
     cy.contains("Chicago, Illinois, United States").should("be.visible");
   });
 
-  it("should allow booking a ride", () => {
+  it("should allow booking a ride as a passenger", () => {
+    cy.intercept("GET", "https://api.mapbox.com/geocoding/v5/mapbox.places/*", {
+      statusCode: 200,
+      body: {
+        features: [
+          {
+            center: [-85.139351, 41.079273],
+            place_name: "Fort Wayne, Indiana, United States",
+            context: [{ id: "country.1", text: "United States" }],
+          },
+          {
+            center: [-87.6298, 41.8781],
+            place_name: "Chicago, Illinois, United States",
+            context: [{ id: "country.1", text: "United States" }],
+          },
+        ],
+      },
+    }).as("geocode");
+
+    cy.intercept("POST", "/api/rides", (req) => {
+      expect(req.body).to.deep.include({
+        createdBy: "passenger",
+        origin: "Fort Wayne, Indiana, United States",
+        destination: "Chicago, Illinois, United States",
+        startTime: req.body.startTime,
+      });
+
+      req.reply({
+        statusCode: 201,
+        body: { message: "Ride created successfully!" },
+      });
+    }).as("createRide");
+
     cy.visit("https://ridelink-public.vercel.app/rides/requestride");
-    cy.get('input[name="rideId"]').type("1");
-    cy.get('button[type="submit"]').click();
-    cy.contains("Booking Successful").should("be.visible");
+
+    cy.get('input[placeholder="Enter pick-up location"]').type("Fort Wayne");
+    cy.wait("@geocode");
+    cy.contains("Fort Wayne, Indiana, United States").click();
+
+    cy.get('input[placeholder="Enter drop-off location"]').type("Chicago");
+    cy.wait("@geocode");
+    cy.contains("Chicago, Illinois, United States").click();
+
+    const futureDate = new Date();
+    futureDate.setDate(futureDate.getDate() + 1);
+    const futureDateTime = `${futureDate.toISOString().split("T")[0]}T10:00`;
+    cy.get('input[type="datetime-local"]').type(futureDateTime);
+    cy.contains("Create Ride").click();
+    cy.wait("@createRide", { timeout: 10000 });
+    cy.url().should("include", "/passenger/dashboard");
   });
 });
